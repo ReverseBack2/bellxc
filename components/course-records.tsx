deccdf576c3c8ed7b0b698679c8c793259463e5a
IronPapaBear <bearimus@gmail.com>
2026-08-15 22:51:04 -0700

    Deploy CCS class-year records

--- components/course-records.tsx
+++ components/course-records.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+// Course Records groups postseason CCS and State performances by class year.
 import { useMemo, useState } from "react";
 import records from "@/data/course-records.json";
 import styles from "./course-records.module.css";
