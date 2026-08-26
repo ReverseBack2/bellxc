70d6ca2e167a5ce5ac32d104ff3424b46dd23860
IronPapaBear <bearimus@gmail.com>
2026-08-26 09:15:00 -0700

    Wire Instagram mockup into homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import { HomePage } from "@/components/pages";
+import { InstagramHomeSection } from "@/components/instagram-home-section";
 import { SiteShell } from "@/components/site-shell";
 
 export default function Home() {
-  return <SiteShell><HomePage /></SiteShell>;
+  return (
+    <SiteShell>
+      <HomePage />
+      <InstagramHomeSection />
+    </SiteShell>
+  );
 }
