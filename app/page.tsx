736071ac6f38e10246b0b394792ce28debefc101
IronPapaBear <bearimus@gmail.com>
2026-08-26 09:29:15 -0700

    Place Instagram feed in desktop homepage column

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
 import { HomePage } from "@/components/pages";
 import { InstagramHomeSection } from "@/components/instagram-home-section";
 import { SiteShell } from "@/components/site-shell";
+import styles from "./instagram-home-mockup.module.css";
 
 export default function Home() {
   return (
     <SiteShell>
-      <HomePage />
-      <InstagramHomeSection />
+      <div className={styles.homeGrid}>
+        <div className={styles.homeMain}>
+          <HomePage />
+        </div>
+        <div className={styles.instagramColumn}>
+          <InstagramHomeSection />
+        </div>
+      </div>
     </SiteShell>
   );
 }
